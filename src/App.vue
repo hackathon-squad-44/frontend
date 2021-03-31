b7808cf9cce674327a4966d5201927683f51ef9b
Rubendvb <rubenvasquez556@gmail.com>
2021-03-31 18:46:24 -0300

    Adicionando front-end

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app" class="container">
+    <Logo />
+  </div>
+</template>
+
+<script>
+import Logo from "./components/Logo";
+
+export default {
+  name: "App",
+  components: {
+    Logo,
+  },
+};
+</script>
+
+<style></style>
