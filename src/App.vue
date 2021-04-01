17dbd70d23320b3e3b3b10ecb1884f033a9c43b6
Rubendvb <rubenvasquez556@gmail.com>
2021-04-01 01:18:43 -0300

    Criando Componente Section

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app" class="container">
     <Header />
+    <Section />
   </div>
 </template>
 
 <script>
 import Header from "./components/Header";
+import Section from "./components/Section";
 
 export default {
   name: "App",
   components: {
     Header,
+    Section,
   },
 };
 </script>
