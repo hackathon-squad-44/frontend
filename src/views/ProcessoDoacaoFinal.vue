f9bec0344eb58e0e331f7049c8b7cd5c8a057ee7
Rubendvb <rubenvasquez556@gmail.com>
2021-04-05 05:27:29 -0300

    Componente Processo Doação Final

---
+++ src/views/ProcessoDoacaoFinal.vue
@@ -0,0 +1,105 @@
+<template>
+  <div>
+    <div class="container">
+      <div class="row ">
+        <div class="col-6 mx-auto mb-4 mt-5 ">
+          <div class="mx-auto navTamanho">
+            <a class="mx-auto fonteLogo navbar-brand" href="index.html">
+              <p class="fonteLogo setarMargin">Mochila cheia</p>
+              <img
+                class="logo ms-4"
+                src="../assets/imgs/logo.svg"
+                alt="Mochila Cheia"
+              />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- BarNav -->
+    <div class="container">
+      <div class="row">
+        <ul class="nav nav-tabs mb-5 ">
+          <div class="col-3">
+            <li class="nav-item">
+              <a class="nav-link" href="index.html">Inicio</a>
+            </li>
+          </div>
+          <div class="col-3">
+            <li class="nav-item">
+              <a class="nav-link" href="nossaCausa.html">Nossa Causa</a>
+            </li>
+          </div>
+          <div class="col-3">
+            <li class="nav-item">
+              <a class="nav-link " href="contatos.html">Contato</a>
+            </li>
+          </div>
+          <div class="col-3">
+            <li class="nav-item px-2 mt-1">
+              <a
+                class="btn  verdeLetra btn-outline-primary ml-md-2 mb-2 px-2"
+                href="login.html"
+                >LOGIN</a
+              >
+            </li>
+          </div>
+        </ul>
+      </div>
+    </div>
+    <!-- BarNav -->
+    <main>
+      <section class="conatiner ">
+        <div class="row">
+          <div class="col-6 mx-auto  pt-5 pb-5 text-start ">
+            <h2 class="manrope mx-auto">
+              Parabéns e muito obrigado por colaborar com a nossa plataforma!
+            </h2>
+          </div>
+        </div>
+      </section>
+      <section class="conatiner ">
+        <div class="row">
+          <div class="col-6 mx-auto pt-5">
+            <p class="manrope2 mb-5 text-start">
+              Você poderá entregar sua doação na escola com o endereço abaixo:
+            </p>
+          </div>
+        </div>
+      </section>
+      <section class="conatiner ">
+        <div class="row">
+          <div class="col-6 mx-auto  ">
+            <p class="manrope2 mb-5 text-start">
+              End: R. Fcamara, 84 - Jardim dos UX - DevCity
+            </p>
+          </div>
+        </div>
+        <section class="conatiner ">
+          <div class="row">
+            <div class="col-6 mx-auto  mb-5">
+              <p class="manrope2 mb-5 text-start">
+                ID de doação: 17839789739
+              </p>
+            </div>
+          </div>
+        </section>
+      </section>
+    </main>
+    <div class=" container-fluid backVerde">
+      <div class="container">
+        <div class="row">
+          <p class="copy roxoLetra pt-3 mx-auto">
+            © 2021 Mochila Cheia. Todos os direitos reservados
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
