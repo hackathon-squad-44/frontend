88df5d612edd6a9d9191503481a6fd112b5d41fe
Rubendvb <rubenvasquez556@gmail.com>
2021-04-05 04:51:51 -0300

    Componente Contato

--- src/views/Contato.vue
+++ src/views/Contato.vue
@@ -1,6 +1,100 @@
 <template>
   <div>
-    <h1>Contato</h1>
+    <div class="container">
+      <div class="row ">
+        <div class="col-6 mx-auto mb-4 mt-5 ">
+          <div class="mx-auto navTamanho">
+            <a class="mx-auto fonteLogo navbar-brand" href="index.html">
+              <p class="fonteLogo setarMargin">Mochila cheia</p>
+              <img
+                class="logo ms-4"
+                src="../assets/imgs/logo.svg"
+                alt="Mochila Cheia"
+              />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <div class="row">
+        <ul class="nav nav-tabs mb-5 ">
+          <div class="col-3">
+            <li class="nav-item">
+              <a class="nav-link" href="index.html">Inicio</a>
+            </li>
+          </div>
+          <div class="col-3">
+            <li class="nav-item">
+              <a class="nav-link" href="nossaCausa.html">Nossa Causa</a>
+            </li>
+          </div>
+          <div class="col-3">
+            <li class="nav-item">
+              <a class="nav-link " href="contatos.html">Contato</a>
+            </li>
+          </div>
+          <div class="col-3">
+            <li class="nav-item px-2 mt-1">
+              <a
+                class="btn  verdeLetra btn-outline-primary ml-md-2 mb-2 px-2"
+                href="login.html"
+                >LOGIN</a
+              >
+            </li>
+          </div>
+        </ul>
+      </div>
+    </div>
+    <main>
+      <section class="conatiner ">
+        <div class="row">
+          <div class="col-12 ms-2 pt-5 pb-5 text-center ">
+            <h2 class="manrope h2Index mx-auto">
+              Entre em contato com o projeto Mochila Cheia
+            </h2>
+          </div>
+        </div>
+      </section>
+      <section class="conatiner ">
+        <div class="row">
+          <div class="col-6 mx-auto pt-5">
+            <p class="manrope2 mb-5 text-start">
+              Tem alguma duvida, sugestão ou quer falar diretamente com a gente?
+              Aqui embaixo está os nossos meios de contato.
+            </p>
+          </div>
+        </div>
+      </section>
+      <section class="conatiner ">
+        <div class="row">
+          <div class="col-6 mx-auto  ">
+            <p class="manrope2 mb-5 text-start">
+              <img src="../assets/imgs/email.svg" alt="" />
+              projetomcheia@gmail.com
+            </p>
+          </div>
+        </div>
+      </section>
+      <section class="conatiner ">
+        <div class="row">
+          <div class="col-6 mx-auto  mb-5">
+            <p class="manrope2 mb-5 text-start">
+              <img src="../assets/imgs/tel.svg" alt="" /> (13) 15253-0599
+            </p>
+          </div>
+        </div>
+      </section>
+    </main>
+    <div class=" container-fluid backVerde">
+      <div class="container">
+        <div class="row">
+          <p class="copy roxoLetra pt-3 mx-auto">
+            © 2021 Mochila Cheia. Todos os direitos reservados
+          </p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
