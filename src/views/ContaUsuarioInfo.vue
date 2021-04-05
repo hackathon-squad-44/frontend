94cffeffe1d8bf8077e2b29bf078b4c9083b423b
Rubendvb <rubenvasquez556@gmail.com>
2021-04-05 05:06:49 -0300

    Componente InfoUsuario

---
+++ src/views/ContaUsuarioInfo.vue
@@ -0,0 +1,174 @@
+<template>
+  <div>
+    <div class="container">
+      <div></div>
+      <div class="row ">
+        <div class="col-6 mx-auto mb-4 mt-5 ">
+          <div class="mx-auto navTamanho">
+            <a class="mx-auto fonteLogo navbar-brand" href="index.html">
+              <p class="fonteLogo setarMargin">Mochila cheia</p>
+              <img
+                class="logo ms-4"
+                src="../assets/imgs/logo.svg"
+                alt="Mochila Cheia"
+              />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- BarNav -->
+    <div class="container">
+      <div class="row">
+        <ul class="nav nav-tabs ">
+          <div class="col-3">
+            <li class="nav-item">
+              <a class="nav-link" href="infos.html">Inicio</a>
+            </li>
+          </div>
+          <div class="col-3">
+            <li class="nav-item">
+              <a class="nav-link" href="infos.html">Nossa Causa</a>
+            </li>
+          </div>
+          <div class="col-3">
+            <li class="nav-item">
+              <a class="nav-link " href="infos.html">Contato</a>
+            </li>
+          </div>
+          <div class="col-3">
+            <li class="nav-item px-2 mt-1">
+              <a
+                class="btn  verdeLetra btn-outline-primary ml-md-2 mb-2 px-2"
+                href="login.html"
+                >Sair</a
+              >
+            </li>
+          </div>
+        </ul>
+      </div>
+    </div>
+    <!-- BarNav -->
+    <main>
+      <section class="container-sm col-12 ">
+        <div class="row">
+          <div class=" container-md col-12  pt-5 setarMargin ">
+            <p class="teste roxoLetra setarPadding mx-auto">
+              Nome do usuario
+            </p>
+          </div>
+        </div>
+      </section>
+      <div class="container">
+        <div class="col-6 mb-3 ">
+          <input
+            type="submit"
+            class="col-6 btn fotoTamanho2 botaoVerde text-white"
+            id=""
+          />
+          <label
+            class="input-group-text fotoTamanho2 botaoVerde text-white"
+            for=""
+            >Carregar foto</label
+          >
+        </div>
+        <ul class="nav nav-pills nav-fill">
+          <li class="nav-item">
+            <a class="nav-link " aria-current="page" href="contaUsuario.html"
+              >Doar</a
+            >
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="contaUsuarioCadastrarDoacao.html"
+              >Cadastrar estudantes</a
+            >
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" href="contaUsuarioInfo.html">Infos</a>
+          </li>
+        </ul>
+      </div>
+      <section class="container-sm col-12">
+        <form class="row  needs-validation" novalidate>
+          <div class="col-md-6 position-relative mt-5">
+            <label for="validationTooltip01" class="form-label roxoLetra"
+              >Nome</label
+            >
+            <input
+              type="text"
+              class="form-control verdeLetra"
+              id="validationTooltip01"
+              value=""
+              required
+            />
+            <div class="valid-tooltip">
+              Looks good!
+            </div>
+          </div>
+          <div class="col-md-6 position-relative mt-5">
+            <label for="validationTooltip02" class="form-label roxoLetra"
+              >E-mail</label
+            >
+            <input
+              type="text"
+              class="form-control verdeLetra"
+              id="validationTooltip02"
+              value=""
+              required
+            />
+            <div class="valid-tooltip">
+              Looks good!
+            </div>
+          </div>
+          <div class="col-md-4 position-relative mt-5">
+            <label for="validationTooltip05 " class="form-label roxoLetra"
+              >Senha</label
+            >
+            <input
+              type="text"
+              class="form-control verdeLetra"
+              id="validationTooltip05"
+              required
+            />
+            <div class="invalid-tooltip">
+              Entre com um CEP válido.
+            </div>
+          </div>
+          <section class="container-sm col-12 ">
+            <div class="row">
+              <div
+                class=" container-md col-12 mb-3  setarPadding  form-check  mt-5"
+              >
+                <p class="teste mt-2 ps-2 roxoLetra setarMargin">
+                  <a href="../views/index.html">Sair da minha conta</a
+                  ><a href="../views/index.html"
+                    ><button
+                      type="button"
+                      class="btn-close ms-2git s"
+                      aria-label="Close"
+                    ></button
+                  ></a>
+                </p>
+              </div>
+            </div>
+          </section>
+        </form>
+      </section>
+    </main>
+    <div class=" container-fluid backVerde">
+      <div class="container">
+        <div class="row">
+          <p class="copy roxoLetra pt-3 mx-auto">
+            © 2021 Mochila Cheia. Todos os direitos reservados
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
