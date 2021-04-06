a57cf9e8d886292975841a29ad6393e5cfd25f48
Vitor Rossini <rossinivitor@hotmail.com>
2021-04-05 23:53:42 -0300

    lista escola do jeito que deu

--- src/services/escola.js
+++ src/services/escola.js
@@ -4,4 +4,10 @@ export default {
   listar: () => {
     return http.get("school");
   },
+  listarNome: (nome) => {
+    return http.get("school/seach?name=" + nome);
+  },
+  listarCidade: (cidade) => {
+    return http.get("school/seach/city?city=" + cidade);
+  },
 };
